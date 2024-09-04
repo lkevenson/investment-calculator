e7cef7ae982b73d8923c296984a242799233b86c
Kevenson Leger <leger.kevenson@gmail.com>
2024-09-03 21:08:09 -0400

    Use of NgModule

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,9 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from './header/header.component';
-import { InvestmentResultsComponent } from './investment-results/investment-results.component';
-import { UserInputComponent } from './user-input/user-input.component';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
+  // standalone: true,
   templateUrl: './app.component.html',
-  imports: [HeaderComponent, UserInputComponent, InvestmentResultsComponent],
+  // imports: [HeaderComponent, UserInputComponent, InvestmentResultsComponent],
 })
 export class AppComponent {}
